4269683e961fbc78fe06299801edff2cbd7709a2
Caesar2934 <119297002+Caesar2934@users.noreply.github.com>
2024-11-15 11:35:55 +0700

    Update Form1.cs

--- GameCaro/Form1.cs
+++ GameCaro/Form1.cs
@@ -4,9 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Net.NetworkInformation;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,158 +12,14 @@ namespace GameCaro
 {
     public partial class Form1 : Form
     {
-        #region Properties
         ChessBoardManager ChessBoard;
-
-        SocketManager socket;
-        #endregion
         public Form1()
         {
             InitializeComponent();
 
-            ChessBoard = new ChessBoardManager(pnlChessBoard, txbPlayerName, pctbMark);
-            ChessBoard.EndedGame += ChessBoard_EndedGame;
-            ChessBoard.PlayerMarked += ChessBoard_PlayerMarked;
-
-            prcbCoolDown.Step = Cons.COOL_DOWN_STEP;
-            prcbCoolDown.Maximum = Cons.COOL_DOWN_TIME;
-            prcbCoolDown.Value = 0;
-
-            tmCoolDown.Interval = Cons.COOL_DOWN_INTERVAL;
-
-            socket = new SocketManager();
-
-            NewGame();
-        }
-
-        #region Methods
-
-        void EndGame()
-        {
-            tmCoolDown.Stop();
-            pnlChessBoard.Enabled = false;
-            undoToolStripMenuItem.Enabled = false;
-            MessageBox.Show("Kết thúc");
-        }
-
-        void NewGame()
-        {
-            prcbCoolDown.Value = 0;
-            tmCoolDown.Stop();
-            undoToolStripMenuItem.Enabled = true;
+            ChessBoard = new ChessBoardManager(pnlChessBoard);
             ChessBoard.DrawChessBoard();
         }
 
-        void Quit()
-        {
-            Application.Exit();
-        }
-
-        void Undo()
-        {
-            ChessBoard.Undo();
-        }
-
-        void ChessBoard_PlayerMarked(object sender, EventArgs e)
-        {
-            tmCoolDown.Start();
-            prcbCoolDown.Value = 0;
-        }
-
-        void ChessBoard_EndedGame(object sender, EventArgs e)
-        {
-            EndGame();
-        }
-
-        private void tmCoolDown_Tick(object sender, EventArgs e)
-        {
-            prcbCoolDown.PerformStep();
-
-            if (prcbCoolDown.Value >= prcbCoolDown.Maximum)
-            {
-                EndGame();                
-            }
-        }
-
-        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            NewGame();
-        }
-
-        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            Undo();
-        }
-
-        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            Quit();            
-        }
-
-        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            if (MessageBox.Show("Bạn có chắc muốn thoát", "Thông báo", MessageBoxButtons.OKCancel) != System.Windows.Forms.DialogResult.OK)
-                e.Cancel = true;
-        }
-        
-        private void btnLAN_Click(object sender, EventArgs e)
-        {
-            socket.IP = txbIP.Text;
-
-            if (!socket.ConnectServer())
-            {
-                socket.CreateServer();
-
-                Thread listenThread = new Thread(() =>
-                {
-                    while (true)
-                    {
-                        Thread.Sleep(500);
-                        try
-                        {
-                            Listen();
-                            break;
-                        }
-                        catch
-                        {
-
-                        }
-                    }
-                });
-                listenThread.IsBackground = true;
-                listenThread.Start();
-            }
-            else
-            {
-                Thread listenThread = new Thread(() =>
-                {
-                    Listen();
-                });
-                listenThread.IsBackground = true;
-                listenThread.Start();
-
-                socket.Send("Thông tin từ Client");
-            }
-
-        }
-
-        private void Form1_Shown(object sender, EventArgs e)
-        {
-            txbIP.Text = socket.GetLocalIPv4(NetworkInterfaceType.Wireless80211);
-
-            if (string.IsNullOrEmpty(txbIP.Text))
-            {
-                txbIP.Text = socket.GetLocalIPv4(NetworkInterfaceType.Ethernet);
-            }
-        }
-
-        void Listen()
-        {
-            string data = (string)socket.Receive();
-
-            MessageBox.Show(data);
-        }
-
-        #endregion
     }
 }
